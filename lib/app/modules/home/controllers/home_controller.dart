edfdbd7d16739fc52ab921d71b909aff230f3966
azizfiqri <aziz.fiqri@gmail.com>
2021-08-20 17:23:41 +0700

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 import 'package:ongkir/app/data/user_model.dart';
@@ -6,11 +7,62 @@ import 'package:ongkir/app/data/user_model.dart';
 class HomeController extends GetxController {
   var ishidden = true.obs;
   var kodeGerbang = 0.obs;
+  var kodeGardu = 0.obs;
+  var ipPCS = "".obs;
+  var namaGerbang = "".obs;
   var nppPetugas = 0.obs;
-
   final count = 0.obs;
+
+  late TextEditingController nppC;
+
+  void getNamaGerbang() async {
+    Uri url = Uri.parse("http://36.92.153.53:88/jm_opsNew/main/login");
+    try {
+      final response = await http.post(
+        url,
+        body: {
+          "npp_no": "${nppPetugas.value}",
+          "password": "${nppPetugas.value}",
+        },
+      );
+      var namagerbang = json.decode(response.body)["data"]["gerbang_nama"];
+      namaGerbang.value = namagerbang;
+      var kodefikasigerbang = json.decode(response.body)["data"]["gerbang_id"];
+      kodeGerbang.value = int.parse(kodefikasigerbang);
+      if (kodefikasigerbang != null) {
+        ishidden.value = false;
+      } else {
+        ishidden.value = false;
+      }
+      print(kodefikasigerbang);
+    } catch (err) {
+      print(err);
+      return print(err);
+    }
+  }
+
+  void openALB() async {
+    print(nppPetugas);
+    Uri url = Uri.parse("http://36.92.153.53:88/jm_opsNew/main/req_alb");
+    try {
+      final response = await http.post(
+        url,
+        body: {
+          "gerbang": "${kodeGerbang.value}",
+          "gardu_id": "${kodeGardu.value}",
+          "tgl_lap": "2021-03-19",
+          "npp_no": "${nppPetugas.value}",
+        },
+      );
+    } catch (err) {
+      print(err);
+      return print(err);
+    }
+  }
+
   @override
   void onInit() {
+    nppC = TextEditingController();
     super.onInit();
   }
 
@@ -20,6 +72,8 @@ class HomeController extends GetxController {
   }
 
   @override
-  void onClose() {}
-  void increment() => count.value++;
+  void onClose() {
+    nppC.dispose();
+    super.onClose();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ongkir/app/modules/home/views/widgets/gardu.dart';
 import 'package:ongkir/app/modules/home/views/widgets/gerbang.dart';
+import 'package:ongkir/app/modules/home/views/widgets/petugas.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -18,61 +19,42 @@ class HomeView extends GetView<HomeController> {
         child: ListView(
           padding: EdgeInsets.all(20),
           children: [
-            nppPetugas(cssFieldController: cssFieldController),
+            Obx(
+              () => Text("Nama Gerbang : ${controller.namaGerbang.value}"),
+            ),
+            Obx(
+              () => Text("Kode Gardu : ${controller.kodeGardu.value}"),
+            ),
+            Obx(
+              () => Text("Kode Gardu : ${controller.ipPCS.value}"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            nppPetugas(),
+            gerbangWidget(),
             SizedBox(
               height: 5.0,
             ),
-            Obx(() => Text("Nama Gerbang : ${controller.nppPetugas.value}")),
-            Text("Petugas CSS :"),
-            Text("Petugas CS :"),
-            gerbangWidget(),
             Obx(
               () => controller.ishidden.isTrue
                   ? SizedBox()
                   : garduWidget(gerbangId: controller.kodeGerbang.value),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () => controller.openALB(),
+                  child: Text('Open ALB'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Req EOP'),
+                ),
+              ],
+            ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class nppPetugas extends GetView<HomeController> {
-  const nppPetugas({
-    Key? key,
-    required this.cssFieldController,
-  }) : super(key: key);
-
-  final TextEditingController cssFieldController;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      onChanged: (value) {
-        if (value != null) {
-          controller.nppPetugas.value = int.parse(value);
-          print(controller.nppPetugas.value);
-        } else {
-          print("kosong");
-        }
-        ;
-      },
-      maxLength: 40,
-      cursorColor: Colors.green,
-      controller: cssFieldController,
-      decoration: InputDecoration(
-        // hintText: "Tulis Pesan",
-        labelText: "NPP Petugas ...",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: BorderSide(
-            color: Colors.green,
-            width: 2.0,
-          ),
         ),
       ),
     );
